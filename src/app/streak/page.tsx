1cce84083cc59172d217a108eb8f4d6f363b03b3
Tamo <tamasbelinszky@gmail.com>
2023-09-23 23:35:28 +0200

    fix icon

--- src/app/streak/page.tsx
+++ src/app/streak/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
       <div className="flex flex-col gap-2 lg:w-1/2">
         <div className="mb-2 flex flex-col items-center justify-center gap-2 lg:mb-8 lg:flex-row lg:justify-between">
           <Link href={"/"} className="flex items-center gap-2 text-end text-2xl font-bold">
-            <Image className="hidden lg:block" src={"/../icon.png"} width={64} height={64} alt="thestreakapp_icon" />
+            <Image className="hidden lg:block" src={"/icon.png"} width={64} height={64} alt="thestreakapp_icon" />
             TheStreakApp
           </Link>
 
