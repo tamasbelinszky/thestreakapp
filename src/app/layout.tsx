84172485aeb9a2f80fda8ae8e4c4932359de34ee
Tamo <tamasbelinszky@gmail.com>
2023-09-23 19:14:25 +0200

    add posthog and gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
+import { Suspense } from "react";
 
 import AuthContext from "./AuthContext";
+import { PHProvider, PostHogPageview } from "./PostHog";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,11 +17,31 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main className="flex min-h-screen w-full flex-col p-4 lg:p-8">
-          <AuthContext>{children}</AuthContext>
-        </main>
-      </body>
+      <Suspense>
+        <Script id="google-tag-manager">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_CONTAINER_ID}')`}
+        </Script>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body className={inter.className}>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_CONTAINER_ID}`}
+              height="0"
+              width="0"
+              className="invisible hidden"
+            ></iframe>
+          </noscript>
+          <main className="flex min-h-screen w-full flex-col p-4 lg:p-8">
+            <AuthContext>{children}</AuthContext>
+          </main>
+        </body>
+      </PHProvider>
     </html>
   );
 }
