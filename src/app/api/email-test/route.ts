28980142755d81571bfb8efe387ef67347e94418
Tamo <tamasbelinszky@gmail.com>
2023-10-27 11:11:28 +0200

    audit

--- src/app/api/email-test/route.ts
+++
@@ -1,8 +0,0 @@
-import { sendTestEmail } from "@/lib/ses";
-
-export async function POST(_request: Request) {
-  // TODO: remove
-  await sendTestEmail();
-
-  return new Response(null, { status: 204 });
-}
